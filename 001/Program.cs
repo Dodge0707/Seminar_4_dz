ccb61ce9a5d4c69cfeb3a5fe5a3749a0e692d5c0
«Vova <vrobskiy@gmail.com>
2022-04-23 18:08:18 +0300

    add example 001. stepen a b

---
+++ 001/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.WriteLine("Введите число А: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число B: ");
+int b = Convert.ToInt32(Console.ReadLine());
+
+int result = Stepen(a, b);
+Console.WriteLine(result);
+
+
+int Stepen(int x, int y)
+{
+      int vixod = 1;
+    for (int i = 1; i <= y; i++)
+    {
+        
+        vixod = vixod * x;
+    }
+    return vixod;
+}
+
+
