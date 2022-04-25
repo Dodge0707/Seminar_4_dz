1e535be9d8bd4170db92d87030280fcb68ad1ddd
«Vova <vrobskiy@gmail.com>
2022-04-25 20:12:54 +0300

    add example finish

--- 003/Program.cs
+++ 003/Program.cs
@@ -1,4 +1,4 @@
-﻿Console.WriteLine("Введите значения массива через запятую");
+﻿/*Console.WriteLine("Введите значения массива через запятую");
 
 string array = Console.ReadLine();
 
@@ -7,4 +7,16 @@ while(index<array.Length)
 {
     Console.Write( array[index]+ " ");
     index+=2 ;
+}*/
+
+int[] numbers = new int[8];
+for (int i = 0; i < numbers.Length; i++)
+{
+    Console.Write($"Введите число {i + 1}: ");
+    numbers[i] = Convert.ToInt32(Console.ReadLine());
+}
+
+for (int i = 0; i < numbers.Length; i++)
+{
+    Console.Write(numbers[i] + " ");
 }
