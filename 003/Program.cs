6e72662a3aa31286aee0f501e77ee36f1786f08d
«Vova <vrobskiy@gmail.com>
2022-04-25 18:34:58 +0300

    add example 004 ready Please enter the commit message for your changes. Lines starting

--- 003/Program.cs
+++ 003/Program.cs
@@ -1,1 +1,10 @@
-﻿
+﻿Console.WriteLine("Введите значения массива через запятую");
+
+string array = Console.ReadLine();
+
+int index = 0;
+while(index<array.Length)
+{
+    Console.Write( array[index]+ " ");
+    index+=2 ;
+}
